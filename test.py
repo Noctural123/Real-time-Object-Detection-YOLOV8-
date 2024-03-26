ebc2ca25246056f5d5cf114c8eac23129b1ffa0c
An Nguyen <hongan.nguyen04@gmail.com>
2024-03-26 00:18:17 -0500

    Real-time detection works, yet still laggy

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hi")
+from ultralytics import YOLO
+from ultralytics.models.yolo.detect.predict import DetectionPredictor
+import cv2
+
+print("Im Running")
+model = YOLO("yolov8x.pt")
+
+results = model.predict(source="0", show=True)
+
+print(results)
